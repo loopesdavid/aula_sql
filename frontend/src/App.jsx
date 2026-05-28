cb79a4d0cd6b2b669273b167ca87a387a31a1852
loopesdavid <loopesdavid@gmail.com>
2026-05-28 20:32:29 +0000

    Segunda alteração

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,48 @@ function App(){
   const [editando, setEditando] = useState(false)
   const [idAtual, setIdAtual] = useState(null)
 
+  async function buscarAlunos(){
+    const resposta = await axios.get('https://fluffy-capybara-g4j5wg747w72wqj-3001.app.github.dev/alunos')
+    setAlunos(resposta.data)
+  }
+
+  useEffect(()=>{buscarAlunos();},[])
+
+  async function salvar(e) {
+    e.preventDefault()
+    const aluno = [nome, email, curso]
+
+    if(editando){
+      await axios.put(`https://fluffy-capybara-g4j5wg747w72wqj-3001.app.github.dev/alunos/${idAtual}`, aluno)
+      setEditando(false)
+      setIdAtual(null)
+    } else {
+      await axios.post('https://fluffy-capybara-g4j5wg747w72wqj-3001.app.github.dev/alunos', aluno)
+    }
+
+    limparFormulario()
+    buscarAlunos()
+  }
+
+  async function excluir(id) {
+    await axios.delete(`https://fluffy-capybara-g4j5wg747w72wqj-3001.app.github.dev/alunos/${id}`)
+    buscarAlunos()
+  }
+
+  function limparFormulario(){
+    setNome('')
+    setEmail('')
+    setCurso('')
+  }
+
+  function editar(aluno){
+    setNome(aluno.nome)
+    setEmail(aluno.email)
+    setCurso(aluno.curso)
+    setIdAtual(aluno.id)
+    setEditando(true)
+  }
+
   return(
     <div style={{padding:20}}>
       <h1>CRUD de Alunos</h1>
@@ -42,6 +84,21 @@ function App(){
           {editando ? "Atualizar" : "Cadastrar"}
         </button>
       </form>
+
+      <hr/>
+
+      {
+        alunos.map((aluno)=>(
+          <div key={aluno.id}>
+            <h3>{aluno.nome}</h3>
+            <p>{aluno.email}</p>
+            <p>{aluno.curso}</p>
+            <button onClick={()=> editar(aluno)}>Editar</button>
+            <button onClick={()=> excluir(aluno.id)}>Excluir</button>
+            <hr/>
+          </div>
+        ))
+      }
     </div>
   )
 }
